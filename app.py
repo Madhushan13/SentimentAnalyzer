3e7688f23957366986808b815d09b6a008dbf293
Madhushan13 <96137093+Madhushan13@users.noreply.github.com>
2024-01-31 01:13:29 +0530

    Add logger

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request, redirect
 from My_helper import preprocessing, vectorizer, get_prediction
+from logger import logging
 app = Flask(__name__)
 
-
+logging.info("Flask server start" )
 data = dict()
 reviews = []
 positive = 0
@@ -13,6 +14,7 @@ def index():
     data["reviews"] = reviews
     data["positive"] = positive
     data["negative"] = negative
+    logging.info("Open Home page" )
     return render_template("index.html", data=data)
 
 @app.route("/", methods = ['post'])
@@ -20,9 +22,13 @@ def my_post():
     global positive, negative 
 
     text = request.form['text']
+    logging.info(f'Text : {text}')
     preprocessed_txt = preprocessing(text)
+    logging.info(f'preprocessed Text : {preprocessed_txt}' )
     vectorized_txt = vectorizer(preprocessed_txt)
+    logging.info(f'vectorized Text : {vectorized_txt}' )
     prediction = get_prediction(vectorized_txt)
+    logging.info(f'prediction : {prediction}')
 
     if prediction == 'negative':
         negative += 1
---
+++ logger.py
@@ -0,0 +1,13 @@
+import logging
+import sys
+
+logging.basicConfig(
+    level = logging.INFO,
+    format = "%(asctime)s - %(levelname)s - %(message)s",
+
+    handlers = [
+        logging.FileHandler(filename ='logs.log', mode = 'w'),
+        logging.StreamHandler(sys.stdout)
+    
+    ]
+)
